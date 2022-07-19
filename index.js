a1c340668dfafadcec06e8f80987acb1809ca4f5
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-18 22:35:41 -0400

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
-
+//first route
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
+//second route
+app.get('*', (req,res) => {
 
-app.listen(3000)
+})
+
+//404 page
+app.get('*', (req,res) => {
+    res.send('<h1>404 Page</h1>')
+})
+app.listen(process.env.PORT)
