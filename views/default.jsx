f2e5434bc2622a981c1239e63aedb3dc1da6b4e3
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-19 21:13:26 -0400

    Adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,20 @@
+const React = require('react')
+
+function Def(html) {
+    retun (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.childre}
+            </body>
+        </html>
+    )
+}
+
+
+
+
+
+module.exports = Def
