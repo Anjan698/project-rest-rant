e127dcc490c8221f28e794078c7671f960d8449e
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-22 22:10:33 -0400

    Adding Home Page

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-import express from 'express'
+const express = require('express')
 const app = express()
 
 app.set('view engine', 'jsx')
@@ -8,9 +8,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
-  })
-  
+  res.render('home')
+})
+
 app.get('*', (req, res) => {
   res.send('404 page')
 })
