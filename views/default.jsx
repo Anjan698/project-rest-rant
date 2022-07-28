91a9b0a0b702405f61f598156925516eb0d52ccd
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-27 22:15:01 -0400

    Adding CSS

--- views/default.jsx
+++ views/default.jsx
@@ -4,13 +4,15 @@ function Def (html) {
     return (
         <html>
             <head>
+            <link rel="stylesheet" href="/css/style.css"/>
                 <title>Title</title>
+                
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
+
 module.exports = Def
