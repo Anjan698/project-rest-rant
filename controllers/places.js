c81d16cc8955e48bf67f7553be1fbb03c748db8f
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-30 19:53:17 -0400

    rest-rant-5

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,27 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
 // GET /places
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      res.render('places/new', { places })
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "http://placekitten.com/250/250",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "http://placekitten.com/250/250",
+    },
+  ];
+  res.render("places/new", { places });
+});
+router.get("/new", (req, res) => {
+res.render("places/new");
+});
 
-  })
-  router.get('/new', (req,res) =>{
-    res.render('/places/new')
-  })
-  
-
-module.exports = router
+module.exports = router;
--- views/default.jsx
+++ views/default.jsx
@@ -1,21 +1,18 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-            <link rel="stylesheet" href="/css/style.css"/>
-                <title>Title</title>
-                
-            </head>
-            <body>
-                {html.children}
-                <a href="/places">
-</a>
-
-            </body>
-        </html>
-    )
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <link rel="stylesheet" href="/css/style.css" />
+        <title>Title</title>
+      </head>
+      <body>
+        {html.children}
+        <a href="/places"></a>
+      </body>
+    </html>
+  );
 }
 
-module.exports = Def
+module.exports = Def;
