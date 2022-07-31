75d9671cb2114acdf0aae4394b5f237087eef09b
Anjan698 <101232411+Anjan698@users.noreply.github.com>
2022-07-30 20:15:13 -0400

    Adding Nav Bar

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,28 @@
-const React = require("react");
+const React = require('react')
 
-function Def(html) {
-  return (
-    <html>
-      <head>
-        <link rel="stylesheet" href="/css/style.css" />
-        <title>Title</title>
-      </head>
-      <body>
-        {html.children}
-        <a href="/places"></a>
-      </body>
-    </html>
-  );
+function Def (html) {
+    return (
+        <html>
+            <head>...</head>
+            <body>
+            <nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/places">Places</a>
+    </li>
+    <li>
+      <a href="/places/new">Add Place</a>
+    </li>
+  </ul>
+</nav>
+
+                {html.children}
+            </body>
+        </html>
+    )
 }
 
-module.exports = Def;
+module.exports = Def
